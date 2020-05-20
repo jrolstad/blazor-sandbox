860a23c00e656fcdcb9f0fe5c01a062b28a291ff
Josh Rolstad <jorolsta@microsoft.com>
2020-05-20 16:06:36 -0700

    Adding calls to graph

--- blazor.wasm.ghpages/Program.cs
+++ blazor.wasm.ghpages/Program.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace blazor.wasm.ghpages
 {
@@ -13,15 +15,33 @@ namespace blazor.wasm.ghpages
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Logging.SetMinimumLevel(LogLevel.Warning);
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            RegisterDefaultHttpClient(builder);
+            RegisterGraphHttpClient(builder);
 
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
             });
 
             await builder.Build().RunAsync();
+        }
+
+        private static void RegisterDefaultHttpClient(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        }
+
+        private static void RegisterGraphHttpClient(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddHttpClient("GraphApi", client => 
+                client.BaseAddress = new Uri("https://graph.microsoft.com"))
+                    .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>()
+                    .ConfigureHandler(new[] { "https://graph.microsoft.com" },new[] { "https://graph.microsoft.com/User.Read" }));
+
+
         }
     }
 }
